50e38ae8904070c66aeab2e420446303ce382bfa
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2024-03-13 09:23:55 +0700

    make object and create line

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Image from "next/image";
 import { useEffect, useRef } from "react";
+import Pen from "./pen"
 
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -8,12 +9,9 @@ export default function Home() {
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
-    bezier(canvas, 100, 100, 100, 200, 300, 100, 400, 200)
-    // const ctx = canvas.getContext('2d');
-    // if (ctx) {
-    //   ctx.fillStyle = 'red';
-    //   ctx.fillRect(0, 0, 100, 100);
-    // }
+    const pen = new Pen(canvas);
+    pen.bezier(100, 100, 100, 200, 300, 100, 400, 200);
+    pen.line(100, 100, 400, 200);
   }, []);
 
   return (
@@ -27,46 +25,4 @@ export default function Home() {
       ></canvas>
     </>
   );
-}
-
-// bezier using getImageData and putImageData
-function bezier(
-  canvas: HTMLCanvasElement,
-  p1x: number, p1y: number, 
-  p2x: number, p2y: number, 
-  p3x: number, p3y: number, 
-  p4x: number, p4y: number
-) {
-  //setup data
-  const ctx = canvas.getContext('2d');
-  if (!ctx) return;
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-  //extrak pixel
-  const data = imageData.data;
-  const width = canvas.width;
-  const height = canvas.height;
-
-  //algoritma aseli
-  const len = Math.sqrt(Math.pow(p1x - p2x, 2) + Math.pow(p1y - p2y, 2)) 
-            + Math.sqrt(Math.pow(p2x - p3x, 2) + Math.pow(p2y - p3y, 2)) 
-            + Math.sqrt(Math.pow(p3x - p4x, 2) + Math.pow(p3y - p4y, 2));
-  console.log(len);
-  const u = 1 / len;
-  for (let i = 0; i < len; i++) {
-    const u2 = u * i;
-
-    //rumus bezier
-    const x = Math.pow(1 - u2, 3) * p1x + 3 * Math.pow(1 - u2, 2) * u2 * p2x + 3 * (1 - u2) * Math.pow(u2, 2) * p3x + Math.pow(u2, 3) * p4x;
-    const y = Math.pow(1 - u2, 3) * p1y + 3 * Math.pow(1 - u2, 2) * u2 * p2y + 3 * (1 - u2) * Math.pow(u2, 2) * p3y + Math.pow(u2, 3) * p4y;
-
-    //menggambar pixel
-    const index = Math.floor(y) * width + Math.floor(x);
-    data[index * 4] = 0; //red
-    data[index * 4 + 1] = 0; //green
-    data[index * 4 + 2] = 0; //blue
-    data[index * 4 + 3] = 255; //alpha
-  }
-  //meletakkan semua pixel
-  ctx.putImageData(imageData, 0, 0);
 }
---
+++ app/pen.tsx
@@ -0,0 +1,84 @@
+class Pen {
+  private canvas: HTMLCanvasElement
+  private ctx: CanvasRenderingContext2D | null = null;
+  private width: number = 0;
+  private height: number = 0;
+  private imageData: ImageData | null = null;
+  private data: Uint8ClampedArray | null = null;
+
+  public constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d")
+    this.width = canvas.width;
+    this.height = canvas.height;
+    if (!this.ctx) return;
+    this.imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    this.data = this.imageData.data;
+
+  }
+
+  public line(x1: number, y1: number, x2: number, y2: number) {
+    const dx = Math.abs(x2 - x1);
+    const dy = Math.abs(y2 - y1);
+    const sx = (x1 < x2) ? 1 : -1;
+    const sy = (y1 < y2) ? 1 : -1;
+    let err = dx - dy;
+    let e2;
+    while (true) {
+      const index = Math.floor(y1) * this.width + Math.floor(x1);
+      if (!this.data) return;
+      this.data[index * 4] = 0; //red
+      this.data[index * 4 + 1] = 0; //green
+      this.data[index * 4 + 2] = 0; //blue
+      this.data[index * 4 + 3] = 255; //alpha
+      if ((x1 === x2) && (y1 === y2)) break;
+      e2 = 2 * err;
+      if (e2 > -dy) {
+        err -= dy;
+        x1 += sx;
+      }
+      if (e2 < dx) {
+        err += dx;
+        y1 += sy;
+      }
+    }
+    this.finishing();
+  }
+
+  public bezier(
+    p1x: number, p1y: number, 
+    p2x: number, p2y: number, 
+    p3x: number, p3y: number, 
+    p4x: number, p4y: number
+  ) {
+    //algoritma aseli
+    const len = Math.sqrt(Math.pow(p1x - p2x, 2) + Math.pow(p1y - p2y, 2)) 
+              + Math.sqrt(Math.pow(p2x - p3x, 2) + Math.pow(p2y - p3y, 2)) 
+              + Math.sqrt(Math.pow(p3x - p4x, 2) + Math.pow(p3y - p4y, 2));
+    const u = 1 / len;
+    for (let i = 0; i < len; i++) {
+      const u2 = u * i;
+
+      //rumus bezier
+      const x = Math.pow(1 - u2, 3) * p1x + 3 * Math.pow(1 - u2, 2) * u2 * p2x + 3 * (1 - u2) * Math.pow(u2, 2) * p3x + Math.pow(u2, 3) * p4x;
+      const y = Math.pow(1 - u2, 3) * p1y + 3 * Math.pow(1 - u2, 2) * u2 * p2y + 3 * (1 - u2) * Math.pow(u2, 2) * p3y + Math.pow(u2, 3) * p4y;
+
+      //menggambar pixel
+      const index = Math.floor(y) * this.width + Math.floor(x);
+      if (!this.data) return;
+      this.data[index * 4] = 0; //red
+      this.data[index * 4 + 1] = 0; //green
+      this.data[index * 4 + 2] = 0; //blue
+      this.data[index * 4 + 3] = 255; //alpha
+    }
+    //meletakkan semua pixel
+    this.finishing();
+  }
+  
+  private finishing() {
+    if (!this.ctx || !this.imageData) return;
+    this.ctx?.putImageData(this.imageData, 0, 0);
+  }
+}
+
+export default Pen;
